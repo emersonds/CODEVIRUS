b867858d3f622338c6df180ddf578ba8f0023938
Dylan Emerson <emersonds2868@gmail.com>
2022-01-19 23:48:16 -0700

    Virus Animation and Child Arrays
     - Wrote animation code (in the Move() method) to make the virus rotate and hover up and down that is tweaked via inspector.
     - Made serialized arrays for every spike, mushroom, and donut child so they can be enabled and disabled via array index. Populated in Inspector.

--- CODE VIRUS/Assets/Scripts/Virus.cs
+++ CODE VIRUS/Assets/Scripts/Virus.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class Virus : MonoBehaviour
 {
+    // Parts of the virus
+    [SerializeField]
+    private GameObject[] spikes = new GameObject[10];
+    [SerializeField]
+    private GameObject[] mushrooms = new GameObject[10];
+    [SerializeField]
+    private GameObject[] donuts = new GameObject[10];
+
+    // Rotation/Hovering Values
+    [SerializeField]
+    private float rotSpeed;         // How fast the virus rotates
+    [SerializeField]
+    private float amplitude;        // How high/low the virus moves
+    [SerializeField]
+    private float frequency;        // How fast the virus move up/down
+
+    private Vector3 posOffset = new Vector3();
+    private Vector3 tempPos = new Vector3();
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Store starting position for movement
+        posOffset = transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Move();
+    }
+
+    private void Move()
+    {
+        // Rotate the virus
+        transform.Rotate(new Vector3(0f, rotSpeed, 0f) * Time.deltaTime, Space.World);
+
+        // Move the virus
+        tempPos = posOffset;
+        tempPos.y += Mathf.Sin(Time.fixedTime * Mathf.PI * frequency) * amplitude;
+        transform.position = tempPos;
     }
 }
