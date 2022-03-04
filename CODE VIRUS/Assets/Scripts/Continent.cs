ca5a09a48d1377dbba4d82b8e951f14cd2384f26
Dylan Emerson <emersonds2868@gmail.com>
2022-03-04 01:55:46 -0700

    It. 1: Added neighbor functionality
     - Continents now contain an array of neighbors in the inspector to see which other continents they can infect and how hard it will be to infect them.
     - Infection not yet implemented.

---
+++ CODE VIRUS/Assets/Scripts/Continent.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Continent : MonoBehaviour
+{
+    [SerializeField]
+    public string continentName;
+    [SerializeField]
+    public bool isInfected = false;
+    [SerializeField]
+    public int totalPopulation;
+    [SerializeField]
+    public int infectedCount;
+    [SerializeField]
+    public int deathCount;
+    [SerializeField]
+    public string biome;
+    [SerializeField]
+    public int Test;
+    [SerializeField]
+    public Neighbor[] neighbors;
+
+    private void Start()
+    {
+        if (neighbors.Length > 0 && neighbors[0] != null)
+            Debug.Log(neighbors[0].continent.continentName);
+        else
+            Debug.Log("No neighbor!!");
+    }
+}
---
+++ CODE VIRUS/Assets/Scripts/Neighbor.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Neighbor
+{
+    public string name;
+    public Continent continent;
+    public float distance;
+    public string distanceType;
+}
